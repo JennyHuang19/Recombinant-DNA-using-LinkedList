3af64ffc573adcd72ff3a2700b8abc0fdd0302f9
Yijian Huang <yijian.huang@duke.edu>
2020-09-29 17:25:23 -0400

    created LinkStrand class

---
+++ src/LinkStrand.java
@@ -0,0 +1,97 @@
+public class LinkStrand implements IDnaStrand {
+
+    private class Node {
+        String info;
+        Node next;
+
+        public Node(String s, Node n) {
+            info = s;
+            next = n;
+        }
+    }
+
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+
+    /**
+     * Constructor with argument
+     * @param d
+     *            is a string representing a DNA strand.
+     * @return the new LinkStrand object with a single node
+     * representing the entire strand of DNA.
+     */
+
+    public LinkStrand(String d){
+        initialize(d);
+    }
+
+    /**
+     * Constructor default
+     * @return the new LinkStrand object with a single node
+     * representing the entire strand of DNA.
+     */
+
+    public LinkStrand(){
+        this("");
+    }
+
+    /**
+     * gets total character count of LinkStrand
+     * @return size of the LinkStrand.
+     */
+
+    @Override
+    public long size() {
+        return mySize;
+    }
+
+
+    @Override
+    public void initialize(String source) {
+
+    }
+
+    /**
+     * Get instance of LinkStrand object
+     * @param source is the source of this enzyme
+     * @return LinkStrand object.
+     */
+
+    @Override
+    public IDnaStrand getInstance(String source) {
+        return new LinkStrand(source);
+    }
+
+    /**
+     * Creates one new node and updates
+     * instance variables to maintain class invariants
+     * @param dna
+     *            is the String appended to this strand
+     * @return mutated LinkStrand object.
+     */
+
+    @Overr
+    public IDnaStrand append(String dna) {
+        myLast.next = new Node(dna, null);
+        myLast = myLast.next;
+        mySize += dna.length();
+        myAppends += 1;
+        return this;
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        return null;
+    }
+
+    @Override
+    public int getAppendCount() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+}
